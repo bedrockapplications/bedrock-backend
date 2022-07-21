4840aaf4316911e46ea1df20aef954a74f68e2ba
Adithya Namada <66470145+dannuadithya@users.noreply.github.com>
2022-07-21 21:42:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("express"); 
 const dotenv = require("dotenv").config();
 const connectDb = require("./db");
 const cors = require("cors");
