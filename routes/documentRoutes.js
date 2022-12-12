62aee60b52fe498331e1101e2f50be51695cac97
a-tarun <63898648+a-tarun@users.noreply.github.com>
2022-12-12 17:21:41 +0530

    added s3 upload files method

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
 const {
   uploadDocument,
   getDocuments,
@@ -63,6 +65,41 @@ const meetingupload = multer({
   storage: meetingstorage,
   fileFilter: fileFilter,
 }).single("attachment");
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.S3_ACCESS_KEY,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: process.env.S3_BUCKET_REGION,
+});
+
+const upload = (bucketName) =>
+  multer({
+    storage: multerS3({
+      s3,
+      bucket: bucketName,
+      metadata: function (req, file, cb) {
+        cb(null, { fieldName: file.fieldname });
+      },
+      key: function (req, file, cb) {
+        cb(null,file.originalname);
+      },
+    }),
+  });
+
+const uploadToS3 = (req, res, next) => {
+  const uploadSingle = upload("bedrockapp-media").single(
+    "image"
+  );
+
+  uploadSingle(req, res, async (err) => {
+    if (err)
+      return res.status(400).json({ success: false, message: err.message });
+
+   console.log(req.file);
+
+    res.status(200).json({ data: req.file });
+  });
+};
 
 router.post("/uploadDocument", multiupload, uploadDocument);
 router.get("/getDocs", getDocuments);
